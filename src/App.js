f1bb8909e2f8e2a283fa5a2252507a4d8898bcbe
AnisaKam <105555967+AnisaKam@users.noreply.github.com>
2022-08-14 21:18:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const [mySearch, setMySearch] = useState('');
   const [myRecipes, setMyRecipes] = useState([]);
-  const [wordSubmitted, setWordSubmitted] = useState ('avacado');
+  const [wordSubmitted, setWordSubmitted] = useState ('mushrooms');
 
   useEffect(() => {
     getRecipes()
